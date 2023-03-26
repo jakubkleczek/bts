9e018ef87880ba268bf5f659330739516a0d9a5d
Kuba Kłeczek <jakub.kleczek@icloud.com>
2023-03-26 14:31:11 +0200

    Scoping in Practice

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+function calcAge(birthYear) {
+    const age = 2023 - birthYear;
+
+    function printAge() {
+        let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+        console.log(output);
+
+        if (birthYear >= 1981 && birthYear <= 1996) {
+            var millenial = true;
+            // creating NEW variable with same name as outer scope's variable
+            const firstName = 'Steven';
+
+            // reassigning outer scope's variable
+            output = 'NEW OUTPUT';
+
+            const str = `Oh, and you're a millenial, ${firstName}`;
+            console.log(str);
+
+            function add(a, b) {
+                return a + b;
+            }
+        }
+        console.log(millenial);
+        console.log(output);
+        // console.log(add(2, 3));
+    }
+    printAge();
+    return age;
+}
+
+const firstName = 'Kuba';
+calcAge(1991);
