c3bad52c65844fae6d39fc5809fc2b67ff19472a
Kuba Kłeczek <jakub.kleczek@icloud.com>
2023-03-27 20:01:17 +0200

    Hoisting and TDZ in Practice

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 function calcAge(birthYear) {
     const age = 2023 - birthYear;
 
@@ -32,3 +33,42 @@ function calcAge(birthYear) {
 
 const firstName = 'Kuba';
 calcAge(1991);
+*/
+
+// Variables
+// console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+// Functions
+// console.log(addDecal(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecal(a, b) {
+    return a + b;
+}
+
+const addExpr = function (a, b) {
+    return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+    console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
