3b1d93eff28e1c98d047e82accfff05735a83497
Kuba Kłeczek <jakub.kleczek@icloud.com>
2023-04-04 19:26:57 +0200

    Primitives vs. Objects in Practice

--- script.js
+++ script.js
@@ -160,6 +160,7 @@ var addArrow = (a, b) => {
 addArrow(2, 5, 8, 12);
 */
 
+/*
 let age = 30;
 let oldAge = age;
 age = 31;
@@ -172,6 +173,46 @@ const me = {
 };
 
 const friend = me;
-friend.age = 27;
+friend.age = 29;
+
+const buba = me;
+friend.age = 23;
+
 console.log('Friend:', friend);
 console.log('Me:', me);
+console.log('Buba:', buba);
+*/
+
+// Primitive types
+let lastName = 'Williams';
+let oldlastName = lastName;
+lastName = 'Davis';
+// console.log(lastName, oldlastName);
+
+// Reference types
+const jessica = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age: 27,
+};
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+// console.log('Before marriage:', jessica);
+// console.log('After marriage:', marriedJessica);
+
+// Copying objects
+const jessica2 = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age: 27,
+    family: ['Alice', 'Bob'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('Before marriage:', jessica2);
+console.log('After marriage:', jessicaCopy);
