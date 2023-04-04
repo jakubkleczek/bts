f1d082b2a1bb948d51ee9520a6f739747850cf56
Kuba Kłeczek <jakub.kleczek@icloud.com>
2023-04-04 16:38:26 +0200

    Regular Functions vs. Arrow Functions

--- script.js
+++ script.js
@@ -75,6 +75,7 @@ let y = 2;
 const z = 3;
 */
 
+/*
 // console.log(this);
 
 const calcAge = function (birthYear) {
@@ -109,3 +110,50 @@ matilda.calcAge();
 
 const f = jonas.calcAge;
 f();
+*/
+
+// var firstName = 'Matylda';
+
+const jonas = {
+    firstName: 'Jonas',
+    year: 1991,
+    calcAge: function () {
+        // console.log(this);
+        console.log(2037 - this.year);
+
+        // Solution 1
+        // const self = this; // self or that
+        // const isMillenial = function () {
+        //     console.log(self);
+        //     console.log(self.year >= 1981 && self.year <= 1996);
+        // };
+
+        // Solution 2
+        const isMillenial = () => {
+            console.log(this);
+            console.log(this.year >= 1981 && this.year <= 1996);
+        };
+
+        isMillenial();
+    },
+
+    greet: function () {
+        console.log(`Hey ${this.firstName}`);
+    },
+};
+jonas.greet();
+jonas.calcAge();
+
+// Arguments keyword
+const addExpr = function (a, b) {
+    console.log(arguments);
+    return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+    console.log(arguments);
+    return a + b;
+};
+addArrow(2, 5, 8, 12);
