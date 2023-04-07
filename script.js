388bde693ba1ad4c319a52c2d7c967952c0f2b6d
Kuba Kłeczek <jakub.kleczek@icloud.com>
2023-04-07 11:51:16 +0200

    Data-Structures-Operators

--- script.js
+++ script.js
@@ -182,7 +182,7 @@ console.log('Friend:', friend);
 console.log('Me:', me);
 console.log('Buba:', buba);
 */
-
+/*
 // Primitive types
 let lastName = 'Williams';
 let oldlastName = lastName;
@@ -216,3 +216,34 @@ jessicaCopy.family.push('John');
 
 console.log('Before marriage:', jessica2);
 console.log('After marriage:', jessicaCopy);
+*/
+
+// Data-Structures-Operators
+
+// Data needed for a later exercise
+const flights =
+    '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// Data needed for first part of the section
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0, // Open 24 hours
+            close: 24,
+        },
+    },
+};
