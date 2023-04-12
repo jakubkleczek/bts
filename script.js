aef40ec1f3c071e8dbcf6c869600d7ba818dec0c
Kuba Kłeczek <jakub.kleczek@icloud.com>
2023-04-12 10:59:10 +0200

    The Nullish Coalescing Operator (??)

--- script.js
+++ script.js
@@ -274,6 +274,17 @@ const restaurant = {
     },
 };
 
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+
+/*
+////////////////////////////////////////////
+// Short Circuiting (&& and ||)
 console.log('---- OR ----');
 
 // Use ANY data type, return ANY data type, short-circiuting
@@ -303,7 +314,7 @@ if (restaurant.orderPizza) {
 }
 
 restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
-
+*/
 /*
 // 1) Destructuring
 
