571189cde2bdede93b1cf2184520fe8ff41b1e29
Kuba Kłeczek <jakub.kleczek@icloud.com>
2023-04-12 10:51:21 +0200

    Short Circuiting (&& and ||)

--- script.js
+++ script.js
@@ -274,6 +274,37 @@ const restaurant = {
     },
 };
 
+console.log('---- OR ----');
+
+// Use ANY data type, return ANY data type, short-circiuting
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+// restaurant.numGuests = 23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('---- AND ----');
+console.log(0 && 'jonas');
+console.log(1 && 'jonas');
+
+console.log('Hello' && 23 && null && 'jonas');
+
+// Practical example
+if (restaurant.orderPizza) {
+    restaurant.orderPizza('mushrooms', 'spinach');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+
+/*
 // 1) Destructuring
 
 // SPREAD, beecause on RIGHT side of =
@@ -308,7 +339,7 @@ add(...x);
 
 restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
 restaurant.orderPizza('mushrooms');
-
+*/
 /*
 /////////////////////////////////////////
 // The Spread Operator (...)
