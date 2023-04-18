757b93601301afcb06922e1908e783182db1dad3
Kuba Kłeczek <jakub.kleczek@icloud.com>
2023-04-18 20:41:40 +0200

    Working With Strings - Part 3

--- script.js
+++ script.js
@@ -278,12 +278,60 @@ const restaurant = {
         console.log(otherIngredients);
     },
 };
+/*
+//////////////////////////////////////
+// Working With Strings - Part 3
+// Split and join
+console.log('a+very+nice+string'.split('+'));
+console.log('Jonas Schmedtmann'.split(' '));
 
-const airline = 'TAP Air Portugal';
+const [firstName, lastName] = 'Jonas Schmedtmann'.split(' ');
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
 
+const capitalizeName = function (name) {
+    const names = name.split(' ');
+    const namesUpper = [];
+
+    for (const n of names) {
+        // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+        namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+    }
+    console.log(namesUpper.join(' '));
+};
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('kuba kleczek');
+
+// Padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(20, '+').padEnd(30, '+'));
+console.log('Jonas'.padStart(20, '+').padEnd(30, '+'));
+
+const maskCreditCard = function (number) {
+    const str = number + '';
+    const last = str.slice(-4);
+    return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(472835424));
+console.log(maskCreditCard('3243253653287692'));
+
+// Repeat
+const message2 = 'Bad weather.. All Departures Delayed... ';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+    console.log(`There are ${n} planes in line ${'🛩️'.repeat(n)}`);
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+*/
 /*
 ////////////////////////////////////////////
 //  Working With Strings - Part 2
+const airline = 'TAP Air Portugal';
 
 console.log(airline.toLowerCase());
 console.log(airline.toUpperCase());
