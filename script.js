890e67475e0fbd2aa4469042f1e78b70506b34c7
Kuba Kłeczek <jakub.kleczek@icloud.com>
2023-04-20 18:33:37 +0200

    How Passing Arguments Works: Value vs. Reference

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
 'use strict';
 // 10-Functions
+
+/////////////////////////////////////////////////////
+// How Passing Arguments Works: Value vs. Reference
+const flight = 'LH234';
+const jonas = {
+    name: 'Jonas Schmedtmann',
+    passport: 234243121,
+};
+
+const checkIn = function (flightNum, passenger) {
+    flightNum = 'LH999';
+    passenger.name = 'Mr. ' + passenger.name;
+
+    if (passenger.passport === 234243121) {
+        alert('Check in');
+    } else {
+        alert('Wrong passport');
+    }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+    person.passport = Math.trunc(Math.random() * 10000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+
+/*
 //////////////////////////////
 // Default Parameters
 
@@ -27,6 +63,7 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
+*/
 
 // 09 Data-Structures-Operators
 
