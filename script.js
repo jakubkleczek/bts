d6f6a782ba5896c8bf839d43c5df00fa5b7783bd
Kuba Kłeczek <jakub.kleczek@icloud.com>
2023-05-05 10:59:40 +0200

    Coding Challenge #1

--- script.js
+++ script.js
@@ -33,7 +33,6 @@ const poll = {
     options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
     // This generates [0, 0, 0, 0]. More in the next section 😃
     answers: new Array(4).fill(0),
-
     registerNewAnswer() {
         // Get answer
         const answer = Number(
@@ -44,20 +43,28 @@ const poll = {
             )
         );
         console.log(answer);
-
         // Register answer
         typeof answer === 'number' &&
             answer < this.answers.length &&
             this.answers[answer]++;
-
-        console.log(this.answers);
+        this.displayResults();
+        this.displayResults('string');
+    },
+    displayResults(type = 'array') {
+        if (type === 'array') {
+            console.log(this.answers);
+        } else if (type === 'string') {
+            // Poll results are 13, 2, 4, 1
+            console.log(`Poll results are ${this.answers.join(', ')}`);
+        }
     },
 };
-// poll.registerNewAnswer();
-
 document
     .querySelector('.poll')
     .addEventListener('click', poll.registerNewAnswer.bind(poll));
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
 
 /*
 /////////////////////////////////////////////////////
